f07f201210f9cca62b278e27e8b143501a277527
Jay09990 <jdudhrejiya0990@gmail.com>
2025-10-16 12:15:08 +0530

    feat: Add local API link for development environment in AdminPanel

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -11,6 +11,7 @@ const AdminPanel = () => {
   const API_BASE_URL =
     import.meta.env.VITE_API_BASE_URL ||
     "https://theinsightbit-backend.onrender.com/api/v1";
+    const localLink = 'http://localhost:8000/api/v1';
 
   const handleSubmit = async (e) => {
     e.preventDefault();
